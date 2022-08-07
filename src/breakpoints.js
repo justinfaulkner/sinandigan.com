8aacdddc459e31406d8db60b36db2b937e0fabaf
Justin Faulkner <justinfaulkner@gmail.com>
2022-08-07 15:04:11 -0700

    initial

---
+++ src/breakpoints.js
@@ -0,0 +1,7 @@
+const size = {
+  sm: "1200x",
+};
+const device = {
+  md: `(max-width: ${size.sm})`,
+};
+export default { size, device };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,192 +1,147 @@
 import * as React from "react"
 import type { HeadFC } from "gatsby"
+import { graphql } from 'gatsby'
+import { StaticImage, IGatsbyImageData } from "gatsby-plugin-image"
+import logo from '../images/400w/logo3.png';
+import Gallery from '@browniebroke/gatsby-image-gallery'
+import styled from 'styled-components';
+import email from '../images/email.png';
 
 const pageStyles = {
   color: "#232129",
-  padding: 96,
+  padding: 0,
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const doclistStyles = {
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
+
+
+const logoStyles = {
+  position: 'absolute',
+  top: 0,
+  left: '2em',
+  right: 0,
 }
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
+const emailStyle = {
+  height: 'calc(24px + .75vw)',
+  verticalAlign: '-8px'
+};
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  display: `inline-block`,
-  marginBottom: 24,
-  marginRight: 12,
-}
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
-
-const docLinks = [
-  {
-    text: "TypeScript Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/custom-configuration/typescript/",
-    color: "#8954A8",
-  },
-  {
-    text: "GraphQL Typegen Documentation",
-    url: "https://www.gatsbyjs.com/docs/how-to/local-development/graphql-typegen/",
-    color: "#8954A8",
+const ContentWrapper = styled.div`
+  padding: 1em 2em;
+  display: flex;
+  font-size: calc(16px + .75vw);
+  
+  flex-direction: row;
+  @media only screen and (max-width: 1024px) {
+    flex-direction: column;
   }
-]
 
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative" as "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
+`;
+
+const LeftCol = styled.div`
+  width: 60%;
+  padding-right: 2em;
+  @media only screen and (max-width: 1024px) {
+    width: 100%;
+    padding-right: 0;
+  }
+`;
+
+const RightCol = styled.div`
+  width: 40%;
+  @media only screen and (max-width: 1024px) {
+    width: 100%;
+  }
+`;
+
+const Headline = styled.div` 
+  text-align: center;
+  font-size: calc(20px + .75vw);
+  font-family: 'Times, serif';
+  font-weight: bold;
+  margin-top: 0.25em;
+`;
+
+interface ImageSharpEdge {
+  node: {
+    childImageSharp: {
+      thumb: IGatsbyImageData
+      full: IGatsbyImageData
+    }
+  }
 }
 
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
+interface PageProps {
+  data: {
+    images: {
+      edges: ImageSharpEdge[]
+    }
+  }
+}
 
-const IndexPage = () => {
+
+const IndexPage:  React.FC<PageProps> = ({data}) => {
+
+  const images = data.images.edges.map(({ node }, index) => ({
+    ...node.childImageSharp,
+    // Generate name based on the index as caption.
+    caption: `Image ${index}`,
+  }))
+
+
   return (
     <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={doclistStyles}>
-        {docLinks.map(doc => (
-          <li key={doc.url} style={docLinkStyle}>
-            <a
-              style={linkStyle}
-              href={`${doc.url}?utm_source=starter&utm_medium=ts-docs&utm_campaign=minimal-starter-ts`}
-            >
-              {doc.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-      <ul style={listStyles}>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter-ts`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
-                </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
+      <div>
+        <StaticImage src="../images/pool2.jpg" alt="Sinandigan house" />
+        
+        <div style={logoStyles}>
+          <img src={logo} alt="Sinandigan house" />
+        </div>
+        
+      </div>
+      <Headline>Live next to a coral reef... private beach included</Headline>
+      <ContentWrapper>
+        <LeftCol>
+          <p>Welcome to Casa Buena Vista, Located in Sinandigan, a "suburb" barrio of Puerto Galera.</p>
+          <p>The house is located near the end of the peninsula, close to Sinandigan lighthouse. </p>
+          <StaticImage src="../images/map.png" alt="Map of House Location" />
+          <p>Located 100 feet above the sea, there are spectacular northwest views across the Verde Passage. </p>
+          <p>A spiral staircase leads to a private coral beach with a World Heritage Site reef immediately offshore.</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/CVWWPsynbto" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+          <p>House is located about 3 miles from Sabang beach and 7 miles from Puerto Galera. The 3 bedroom house features air conditioning, hot water, and all appliances, as well as a pool overlooking the ocean</p>
+          <p>For further information regarding long or short term rental, please contact us at <a href="mailto:info@sinandigan.com">info@sinandigan.com</a>.</p>
+        </LeftCol>
+        <RightCol>
+          <Gallery images={images} colWidth={50} mdColWidth={50} />
+        </RightCol>
+        </ContentWrapper>
     </main>
   )
 }
+
+export const pageQuery = graphql`
+  query ImagesForGallery {
+    images: allFile(
+      filter: { relativeDirectory: { eq: "gallery" } }
+      sort: { fields: name }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            thumb: gatsbyImageData(
+              width: 270
+              height: 270
+              placeholder: BLURRED
+            )
+            full: gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
 
